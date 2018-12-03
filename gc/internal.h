c886f64fd62b57ba1fcc34baa31c80422abdbc5f
Theodoros Chatzigiannakis <tchatzigiannakis@gmail.com>
2018-12-03 17:29:35 +0200

    Add some debug printing.

--- gc/internal.h
+++ gc/internal.h
@@ -2,6 +2,7 @@
 
 GC_ALLOC_INFO *GC_resolve_internal(uintptr_t ptr)
 {
+    // printf("Checking %p... ", (void *)ptr);
     if (IN_RANGE(ptr, GC_heap_begin, GC_heap_end))
     {
         for (size_t i = 0; i < GC_object_count; i++)
@@ -9,10 +10,16 @@ GC_ALLOC_INFO *GC_resolve_internal(uintptr_t ptr)
             GC_ALLOC_INFO *info = &GC_allocations[i];
             if (IN_RANGE(ptr, info->address, info->address + info->size))
             {
+                // printf("Yep, it's %p!!!!!!!!!!!!!\n", (void *)info->address);
                 return info;
             }
         }
+        // printf("But it doesn't match any object.\n");
+        return NULL;
     }
-
-    return NULL;
+    else
+    {
+        // printf("Nope, not a heap pointer.\n");
+        return NULL;
+    }
 }
