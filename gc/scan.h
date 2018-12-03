f784be4c089eb598859b78ae665910884c7ac78e
Theodoros Chatzigiannakis <tchatzigiannakis@gmail.com>
2018-12-03 16:25:23 +0200

    Implement scan.

--- gc/scan.h
+++ gc/scan.h
@@ -2,4 +2,17 @@
 
 void GC_scan(uintptr_t min, uintptr_t max)
 {
+    for (uintptr_t i = min; i < max; i++)
+    {
+        GC_ALLOC_INFO *info = GC_resolve_internal(i);
+        if (info && !info->alive)
+        {
+            info->alive = 1;
+            GC_scan(info->address, info->address + info->size);
+        }
+        else
+        {
+            info->alive = 1;
+        }
+    }
 }
