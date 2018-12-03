e781468d7b9667f92d752398a150de3366c743be
Theodoros Chatzigiannakis <tchatzigiannakis@gmail.com>
2018-12-03 17:30:00 +0200

    Fix bug reading from invalid memory.

--- gc/scan.h
+++ gc/scan.h
@@ -4,15 +4,19 @@ void GC_scan(uintptr_t min, uintptr_t max)
 {
     for (uintptr_t i = min; i < max; i++)
     {
-        GC_ALLOC_INFO *info = GC_resolve_internal(i);
-        if (info && !info->alive)
+        uintptr_t ptr = *(uintptr_t *)i;
+        GC_ALLOC_INFO *info = GC_resolve_internal(ptr);
+        if (info)
         {
-            info->alive = 1;
-            GC_scan(info->address, info->address + info->size);
-        }
-        else
-        {
-            info->alive = 1;
+            if (!info->alive)
+            {
+                info->alive = 1;
+                GC_scan(info->address, info->address + info->size);
+            }
+            else
+            {
+                info->alive = 1;
+            }
         }
     }
 }
